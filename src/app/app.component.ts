0be345dbfa739a7c7cb46d07f49cb7fdbb1bcc2f
apurva52 <55528747+apurva52@users.noreply.github.com>
2021-05-02 11:44:10 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -121,7 +121,7 @@ export class AppComponent implements OnInit {
             this.storeData(JSON.stringify(this.finaldata));
             this.getData();
             this.messageService.clear();
-        this.messageService.add({ severity: 'success', summary: 'Updated Successfully', detail: '' });
+        this.messageService.add({ severity: 'success', summary: 'Deleted Successfully', detail: '' });
           }
         });
       }
